5ea43d86da154deef52af0bfc72371ddc7edb788
Saumya Shekhar <saumya.shekhar@sap.com>
2019-09-17 16:32:28 +0530

    AutoWired Java Configuration

--- spring_sample_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -3,20 +3,30 @@ package com.pluralsight.service;
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-    public CustomerServiceImpl(CustomerRepository customerRepository){
-        this.customerRepository = customerRepository;
-    }
-
-    public void setCustomerRepository(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
     private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(){
+
+    }
+
+    public CustomerServiceImpl(CustomerRepository customerRepository){
+        System.out.println("Using Constructor Injection");
+        this.customerRepository = customerRepository;
+    }
+
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("Using Setter Injection");
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll(){
