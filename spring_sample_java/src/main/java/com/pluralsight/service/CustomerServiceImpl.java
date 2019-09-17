6125b2c2ebf4a67c1392e41d0b9ef7a5db4d3ceb
Saumya Shekhar <saumya.shekhar@sap.com>
2019-09-17 16:43:08 +0530

    Enum Constant File Using for Spring Constants

--- spring_sample_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -4,13 +4,14 @@ import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customerService")
-@Scope("singleton")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
