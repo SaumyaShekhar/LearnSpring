15c7c51ba980a4e955df365a061f1f15c3c7b902
Saumya Shekhar <saumya.shekhar@sap.com>
2019-09-17 14:37:26 +0530

    @Configuration Using java

---
+++ spring_sample_java/src/main/java/AppConfig.java
@@ -0,0 +1,13 @@
+import com.pluralsight.service.CustomerService;
+import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "customerService")
+    public CustomerService getCustomerService(){
+        return new CustomerServiceImpl();
+    }
+}
