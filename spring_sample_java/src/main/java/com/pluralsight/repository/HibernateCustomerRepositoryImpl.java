bc3decd4e0b8eb6b2b2e77710f0238ea409ce97e
Saumya Shekhar <saumya.shekhar@sap.com>
2019-09-18 12:27:56 +0530

    Java Config for ReadingProperties

--- spring_sample_java/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ spring_sample_java/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,8 +10,11 @@ import java.util.List;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUserName}")
+    private String dbUserName;
     @Override
     public List<Customer> findAll() {
+        System.out.println(dbUserName);
         List<Customer> customerList = new ArrayList<>();
 
         Customer cust = new Customer();
