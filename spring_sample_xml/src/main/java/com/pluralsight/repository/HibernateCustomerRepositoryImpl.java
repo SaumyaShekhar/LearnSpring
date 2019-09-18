9a69f383821e4a2b5fa6a20ea78dcb8fc56bf3f2
Saumya Shekhar <saumya.shekhar@sap.com>
2019-09-18 11:56:50 +0530

    Reading properties using XML Config

--- spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -7,8 +7,18 @@ import java.util.List;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    public String getDbUserName() {
+        return dbUserName;
+    }
+
+    public void setDbUserName(String dbUserName) {
+        this.dbUserName = dbUserName;
+    }
+
+    private String dbUserName;
     @Override
     public List<Customer> findAll() {
+        System.out.println(dbUserName);
         List<Customer> customerList = new ArrayList<>();
 
         Customer cust = new Customer();
